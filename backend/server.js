8d981e3b4a6ad2db6be8c0e77da91faa111799ef
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-08 15:05:09 +0700

    Add JSON parsing middleware and enhance transaction creation endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json()); //middleware to parse json body
+
+//our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is ", req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -31,7 +39,36 @@ app.get("/", (req, res) => {
   res.send("Hello World 1232");
 });
 
-console.log("My port:", PORT);
+// app.get("/api/transactions", async (req, res) => {
+
+// });
+
+app.post("/api/transactions", async (req, res) => {
+  //titl, amount, category, user_id
+  try {
+    const {title, amount, category, user_id} = req.body;
+
+    if (!title || amount === undefined || !category || !user_id) {
+      return res.status(400).json({message: "All fields are required"});
+    }
+
+    const transaction = await sql`
+      INSERT INTO transactions (title, amount, category, user_id)
+      VALUES (${title}, ${amount}, ${category}, ${user_id})
+      RETURNING *
+    `;
+    console.log("Transaction created:", transaction);
+    res
+      .status(201)
+      .json({
+        message: "Transaction created successfully",
+        transaction: transaction[0],
+      });
+  } catch (error) {
+    console.log("Error creating the transaction", error);
+    res.status(500).json({message: "Server Error"});
+  }
+});
 
 initDB().then(() => {
   app.listen(PORT, () => {
