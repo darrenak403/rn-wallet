ee905dfb4b57b729f1b894341d819571db14cc63
darrenak403 <ngothanhdat4002@gmail.com>
2025-10-09 13:29:31 +0700

    Refactor backend structure: migrate files to src directory, implement database initialization, and restore transaction API functionality

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import {initDB} from "./config/db.js";
+import rateLimiter from "./middleware/rateLimit.js";
+
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(rateLimiter); //apply rate limiter to all the routes
+app.use(express.json()); //middleware to parse json body
+
+const PORT = process.env.PORT || 5001;
+
+app.use("/api/transactions", transactionsRoute);
+// app.use("/api/products", transactionsRoute); //future route
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
